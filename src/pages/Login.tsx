38f98cbe82a23c132253b12df598bdc119f6f647
hugo <ducky4tools@gmail.com>
2024-04-05 16:59:54 +0200

    Dev en cours de la page de login.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,36 +1,33 @@
-import { isNotEmpty, useForm } from '@mantine/form';
-import { TextInput, Box, PasswordInput, Title, Group } from '@mantine/core';
-import CustomizeButton from '../components/shared/CustomizeButton';
+import { Button, Container, PasswordInput, TextInput } from "@mantine/core";
+import CustomizeButton from "../components/shared/CustomizeButton";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
 
-    const form = useForm({
-        initialValues: { email: '', firstname: '', lastname: '', password: '', confirmPassword: '' },
+    const handleRedirectToForgottenPasswordPage = () => {
+        navigate('/resetPassword');
+    }
 
-        validate: {
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Email invalide'),
-            password: isNotEmpty(),
-            confirmPassword: (value, values) =>
-                value !== values.password ? 'Les mots de passe de correspondent pas' : null,
-            firstname: isNotEmpty(),
-            lastname: isNotEmpty(),
-        },
-    });
+    const handleConnect = () => {
+        
+    }
+
+
+    const [validEmail, setValidEmail] = useState(false);
 
     return (
-        <Box maw={340} mx="auto">
-            <Title order={2} lineClamp={2}>Espace d'inscription</Title>
-            <form onSubmit={form.onSubmit(console.log)}>
-                <TextInput withAsterisk mt="sm" label="Email" placeholder="Email" {...form.getInputProps('email')} />
-                <TextInput withAsterisk mt="sm" label="Nom" placeholder="Nom"  {...form.getInputProps('firstname')} />
-                <TextInput withAsterisk mt="sm" label="Prénom" placeholder="Prénom"  {...form.getInputProps('lastname')} />
-                <PasswordInput withAsterisk mt="sm" label="Mot de passe" placeholder="Mot de passe"  {...form.getInputProps('password')} />
-                <PasswordInput withAsterisk mt="sm" label="Confirmation" placeholder="Confirmation" {...form.getInputProps('confirmPassword')} />
-                <Group justify="flex-end" mt="md">
-                    <CustomizeButton text='Incription'>
-                    </CustomizeButton>
-                </Group>
-            </form>
-        </Box>
-    );
+        <>
+            <Container color="red">
+                {!validEmail && (<TextInput label="Email" placeholder="email" />)}
+                {validEmail && (<PasswordInput label="Mot de passe" placeholder="mot de passe" />)}
+                <br />
+                <CustomizeButton onClick={handleConnect} text="Se connecter" />
+                {validEmail && (<Button variant="transparent"
+                    onClick={handleRedirectToForgottenPasswordPage}>Mot de passe oublié ?</Button>)}
+            </Container>
+        </>
+    )
 }
