2c5ef4d4d66ee14982712fbfbcdab79d45bde583
Faxiis <antho.lavoute@gmail.com>
2024-04-10 15:51:20 +0200

    Revert "Ajout Navbar et correction bugs"
    This reverts commit 6aec442528965b3a5e2c5a3459dff5a1b1de113c.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,6 @@ import {Box, Paper, PasswordInput, TextInput, Title} from "@mantine/core";
 import CustomizeButton from "../components/shared/CustomizeButton.tsx";
 import { useNavigate } from "react-router-dom";
 import PasswordStrengthBar from 'react-password-strength-bar';
-import Navbar from "../components/shared/Navbar.tsx";
 
 
 export default function Register() {
@@ -41,10 +40,9 @@ export default function Register() {
     }
 
     return (
-        <Box>
-            <Navbar/>
+        <div>
             <Paper shadow="xl" radius="xl" withBorder p="xl">
-                <Title order={2} lineClamp={2} mb={25}>Espace d'inscription</Title>
+                <Title order={2} lineClamp={2} mb={50}>Espace d'inscription</Title>
                 <Formik
                     initialValues={{
                         username: '',
@@ -147,12 +145,12 @@ export default function Register() {
                                 </CustomizeButton>
                             </Box>
                             <Box >
-                                <CustomizeButton type={"button"} variant="transparent" onClick={() => navigate('/login')} text={"Se connecter"}></CustomizeButton>
+                                <CustomizeButton variant="transparent" onClick={() => navigate('/login')} text={"Se connecter"}></CustomizeButton>
                             </Box>
                         </form>
                     )}
                 </Formik>
             </Paper>
-        </Box>
+        </div>
     );
 }
